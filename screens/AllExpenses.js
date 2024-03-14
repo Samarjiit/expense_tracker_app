5e3516160922bbf2ba6bd6141042c43e4ed274fc
Samarjiit <samarjitmahi6245@gmail.com>
2024-03-14 16:14:17 +0530

    updated code

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,6 +1,16 @@
 import { Text } from "react-native"
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
+import { useContext } from "react"
+import { ExpensesContext } from "../store/expenses-context"
 function AllExpenses() {
-  return <Text>All expenses screen</Text>
+  const expensesCtx = useContext(ExpensesContext)
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!!!"
+    />
+  )
 }
 
 export default AllExpenses
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,6 +1,22 @@
-import { Text } from "react-native"
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
+import { getDateMinusDays } from "../components/ExpensesOutput/util/date"
+import { ExpensesContext } from "../store/expenses-context"
+import { useContext } from "react"
 function RecentExpenses() {
-  return <Text>Recent expenses screen</Text>
+  const expensesCtx = useContext(ExpensesContext)
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date()
+    const date7daysAgo = getDateMinusDays(today, 7)
+
+    return expense.date >= date7daysAgo && expense.date <= today
+  })
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="last 7 days ago"
+      fallbackText="No expenses registered for the last 10 days"
+    />
+  )
 }
 
 export default RecentExpenses
